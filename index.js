196170bb9ec3dce889a86817c58b8fe49394fe20
ivillop <rd.akbar@yandex.com>
2023-10-18 01:14:21 +0700

    addgetwaves and revision interface

--- index.js
+++ index.js
@@ -16,4 +16,29 @@ document.addEventListener('DOMContentLoaded', function () {
   main.addEventListener('click', function () {
     drawer.classList.remove('open');
   });
+
+  // Add scrollspy to <body>
+document.querySelector('body').scrollspy({
+  target: 'nav',
+  offset: 50,
+});
+
+// Add smooth scrolling on all links inside the navbar
+document.querySelectorAll('#drawer a').forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+
+    const hash = link.hash;
+    const target = document.querySelector(hash);
+
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth',
+      });
+
+      window.location.hash = hash;
+    }
+  });
+});
 });
